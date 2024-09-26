5b0baa9d4861c46bbd310529bee58ff3aa490610
Forrest Miller <forrestmillerj@gmail.com>
2024-09-26 14:08:07 -0500

    cleanup

--- src/bot/src/main.rs
+++ src/bot/src/main.rs
@@ -1,11 +1,5 @@
 use std::env;
 
-// SERENITY
-// use serenity::async_trait;
-// use serenity::model::channel::Message;
-// use serenity::prelude::*;
-// struct Handler;
-
 use poise::serenity_prelude as serenity;
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -23,45 +17,13 @@ async fn age(
     Ok(())
 }
 
-// #[async_trait]
-// impl EventHandler for Handler {
-//     async fn message(&self, ctx: serenity::Context, msg: Message) {
-//         if msg.content == "!ping" {
-//             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-//                 println!("Error sending message: {why:?}");
-//             }
-//         }
-//         if msg.content == "!hello" {
-//             if let Err(why) = msg.channel_id.say(&ctx.http, "Hi!").await {
-//                 println!("Error sending message: {why:?}");
-//             }
-//         }
-//     }
-// }
-
 #[tokio::main]
 async fn main() {
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    // SERENITY SETUP
-    // Set gateway intents, which decides what events the bot will be notified about
-    // let intents = GatewayIntents::GUILD_MESSAGES
-    //     | GatewayIntents::DIRECT_MESSAGES
-    //     | GatewayIntents::MESSAGE_CONTENT;
-    //
-    // Create a new instance of the Client, logging in as a bot.
-    // let mut client = Client::builder(&token, intents)
-    //     .event_handler(Handler)
-    //     .await
-    //     .expect("Err creating client");
-    // Start listening for events by starting a single shard
-    // if let Err(why) = client.start().await {
-    //     println!("Client error: {why:?}");
-    // }
-
-    // POISE SETUP
     let intents = serenity::GatewayIntents::non_privileged();
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![age()],
@@ -74,8 +36,10 @@ async fn main() {
             })
         })
         .build();
+
     let client = serenity::ClientBuilder::new(token, intents)
         .framework(framework)
         .await;
+
     client.unwrap().start().await.unwrap();
 }
