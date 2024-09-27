bd413550b2e1b7bc1ab2e954d414ed38a450c2bc
Forrest Miller <forrestmillerj@gmail.com>
2024-09-27 17:57:12 -0500

    fix

--- infra/modules/aws/lambda/_locals.tf
+++ infra/modules/aws/lambda/_locals.tf
@@ -1,5 +1,5 @@
 locals {
   source_code_hash = {
-    dnd_bot = fileexists("${path.module}/bin/hello/bootstrap.zip") ? filebase64sha256("${path.module}/bin/twitch-event-sub/bootstrap.zip") : data.aws_lambda_function.dnd_bot_exists[0].source_code_hash
+    dnd_bot = fileexists("${path.module}/bin/hello/bootstrap.zip") ? filebase64sha256("${path.module}/bin/hello/bootstrap.zip") : data.aws_lambda_function.dnd_bot_exists[0].source_code_hash
   }
 }
