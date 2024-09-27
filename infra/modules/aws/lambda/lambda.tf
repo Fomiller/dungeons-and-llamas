8199008dfa39d6b14554aabfbcc155f145fb6a91
Forrest Miller <forrestmillerj@gmail.com>
2024-09-27 16:19:59 -0500

    update lamda module structure

---
+++ infra/modules/aws/lambda/lambda.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "dnd_bot" {
+  function_name    = "${var.namespace}-${var.app_prefix}-bot"
+  role             = var.iam_role_arn_lambda_twitch_event_sub
+  handler          = "bootstrap"
+  filename         = "${path.module}/bin/dnd_bot/bootstrap.zip"
+  source_code_hash = local.source_code_hash["dnd_bot"]
+  runtime          = "provided.al2"
+  architectures    = ["arm64"]
+  memory_size      = 128
+  timeout          = 10
+  environment {
+    variables = {
+      REGION  = data.aws_region.current.name
+      ACCOUNT = data.aws_caller_identity.current.account_id
+    }
+  }
+}
+
