c6952495d2d3f8fd197378d59043a98a9852109a
Forrest Miller <forrestmillerj@gmail.com>
2024-09-28 16:43:57 -0500

    format

--- infra/modules/aws/api-gateway/main.tf
+++ infra/modules/aws/api-gateway/main.tf
@@ -39,9 +39,9 @@ resource "aws_api_gateway_integration" "lambda" {
 
 # handle root
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -51,18 +51,18 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 resource "aws_api_gateway_deployment" "example" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  rest_api_id = aws_api_gateway_rest_api.example.id
   stage_name  = "test"
 }
 
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_name_hello}"
+  function_name = var.lambda_name_hello
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
