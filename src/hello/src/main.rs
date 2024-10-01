2f9f6d994da337313c8b213192f6c016ffcd2916
Forrest Miller <forrestmillerj@gmail.com>
2024-10-01 14:29:12 -0500

    serverless bot code init

--- src/hello/src/main.rs
+++ src/hello/src/main.rs
@@ -1,25 +1,85 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use lambda_http::{
+    http::StatusCode, run, service_fn, tracing, Body, Error, Request, RequestExt, Response,
+};
+use nacl::sign::verify;
+use serde_json::json;
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+const PUBLIC_KEY: &str = "PUBLIC KEY";
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    println!("EVENT: {:?}", event);
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+    let headers = event.headers();
+    println!("HEADERS: {:?}", headers);
+
+    let body = std::str::from_utf8(event.body()).expect("non utf-8 body");
+    println!("BODY: {:?}", body);
+
+    let timestamp = headers
+        .get("x-signature-timestamp")
+        .expect("missing x-signature-timestamp header")
+        .to_str()
+        .unwrap();
+
+    let signature = headers
+        .get("x-signature-ed25519")
+        .expect("missing x-signature-ed25519 header")
+        .to_str()
+        .unwrap();
+
+    let message = timestamp.to_owned() + body;
+
+    let _ = match verify(
+        message.as_bytes(),
+        signature.as_bytes(),
+        PUBLIC_KEY.as_bytes(),
+    ) {
+        Ok(res) => res,
+        Err(_) => {
+            let resp: Response<Body> = Response::builder()
+                .status(401)
+                .header("content-type", "text/html")
+                .body("invalid request signature".into())
+                .map_err(Box::new)?;
+            return Ok(resp);
+        }
+    };
+
+    let body_json = json!(body);
+    println!("BODY_JSON: {:?}", json!(body_json));
+
+    let msg_type = body_json.get("type").expect("type not found");
+    println!("MSG TYPE: {:?}", msg_type);
+    match msg_type
+        .to_string()
+        .parse::<u32>()
+        .expect("unable to parse type")
+    {
+        1 => {
+            let json = json!({"type": 1});
+            let resp: Response<Body> = Response::builder()
+                .status(200)
+                .body(Body::Text(json.to_string()))
+                .unwrap();
+            return Ok(resp);
+        }
+        2 => {
+            let json = json!({"type": 4,"data": {"content": "Hello, World."}});
+            let resp: Response<Body> = Response::builder()
+                .status(200)
+                .header("content-type", "application/json")
+                .body(Body::Text(json.to_string()))
+                .unwrap();
+            return Ok(resp);
+        }
+        _ => {
+            let resp: Response<Body> = Response::builder()
+                .status(400)
+                .body("unhandled command".into())
+                .unwrap();
+            return Ok(resp);
+        }
+    };
 }
 
 #[tokio::main]
