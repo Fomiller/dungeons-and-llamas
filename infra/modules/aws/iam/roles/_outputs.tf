e039091c9fa32b192308dcc4d3e8ce860927724a
Forrest Miller <forrestmillerj@gmail.com>
2024-10-05 18:03:07 -0500

    spelling

--- infra/modules/aws/iam/roles/_outputs.tf
+++ infra/modules/aws/iam/roles/_outputs.tf
@@ -6,7 +6,7 @@ output "iam_role_name_lambda_dnd_bot" {
   value = aws_iam_role.lambda_dnd_bot.name
 }
 
-output "iam_role_arn_lambda_command_mananger" {
+output "iam_role_arn_lambda_command_manager" {
   value = aws_iam_role.lambda_command_manager.arn
 }
 
