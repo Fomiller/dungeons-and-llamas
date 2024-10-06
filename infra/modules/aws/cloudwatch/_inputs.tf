7647ff2caa5f6ffef2e14f90bf0b1a2b9be4fd66
Forrest Miller <forrestmillerj@gmail.com>
2024-10-06 08:02:45 -0500

    cloudwatch log group init

--- infra/modules/aws/cloudwatch/_inputs.tf
+++ infra/modules/aws/cloudwatch/_inputs.tf
@@ -1,1 +1,2 @@
 variable "lambda_name_hello" { type = string }
+variable "lambda_name_command_manager" { type = string }
--- infra/modules/aws/cloudwatch/main.tf
+++ infra/modules/aws/cloudwatch/main.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "event_sub" {
   name              = "/aws/lambda/${var.lambda_name_hello}"
   retention_in_days = 7
 }
+
+resource "aws_cloudwatch_log_group" "event_sub" {
+  name              = "/aws/lambda/${var.lambda_name_command_manager}"
+  retention_in_days = 7
+}
