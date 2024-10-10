2aa63d29070d532c3ee0e17c6b93883db8529a48
Forrest Miller <forrestmillerj@gmail.com>
2024-10-10 08:27:06 -0500

    dynamo iam perms for lambda

--- infra/modules/aws/iam/policies/attachments.tf
+++ infra/modules/aws/iam/policies/attachments.tf
@@ -3,6 +3,11 @@ resource "aws_iam_role_policy_attachment" "lambda_discord_bot" {
   role       = var.iam_role_name_lambda_discord_bot
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_discord_bot_dynamodb" {
+  policy_arn = aws_iam_policy.dnl_dynamodb_rw.arn
+  role       = var.iam_role_name_lambda_discord_bot
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_discord_command_manager_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = var.iam_role_name_lambda_discord_command_manager
---
+++ infra/modules/aws/iam/policies/dynamodb_rw.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "dynamodb_rw" {
+  statement {
+    sid    = "DyanamoDBReadWtie"
+    effect = "Allow"
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:PutItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.namespace}-${var.app_prefix}*"
+    ]
+  }
+}
+resource "aws_iam_policy" "dnl_dynamodb_rw" {
+  name   = "${title(var.namespace)}${title(var.app_prefix)}DynamoDBReadWrite"
+  policy = data.aws_iam_policy_document.dynamodb_rw.json
+}
+
+
