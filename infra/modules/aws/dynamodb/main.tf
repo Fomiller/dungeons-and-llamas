2e541c2411579aa48cf0455b9486d6e36425fb44
Forrest Miller <forrestmillerj@gmail.com>
2024-10-10 13:55:57 -0500

    add sort_key StateComponent to game state table

--- infra/modules/aws/dynamodb/main.tf
+++ infra/modules/aws/dynamodb/main.tf
@@ -1,13 +1,18 @@
 resource "aws_dynamodb_table" "dnl_game_state" {
   name         = "${var.namespace}-${var.app_prefix}-${var.environment}-game-state"
-  hash_key     = "UserId"
   billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserId"
+  range_key    = "StateComponent"
 
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "UserId"
+    type = "S"
+  }
+  attribute {
+    name = "StateComponent"
     type = "S"
   }
 }
