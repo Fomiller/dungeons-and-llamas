e91692994bd491e6419c8eb1acc086ebc9eae5f1
Forrest Miller <forrestmillerj@gmail.com>
2024-10-14 15:12:09 -0500

    spells sort key tests

--- src/game/src/state/game/player/actions/mod.rs
+++ src/game/src/state/game/player/actions/mod.rs
@@ -1,5 +1,21 @@
+pub mod spells;
+
+use spells::SpellSortKey;
+
 #[derive(strum::Display)]
 pub enum ActionsSortKey {
-    #[strum(to_string = "#Spells#")]
-    Spells,
+    #[strum(to_string = "Spells#{0}")]
+    Spells(SpellSortKey),
+    #[strum(to_string = "#Action")]
+    Action,
+    #[strum(to_string = "#BonusAction")]
+    BonusAction,
+    #[strum(to_string = "#Action")]
+    Reaction,
 }
+// 345345345345#GameState#Player#Actions#Spells#Cantrip
+// 345345345345#GameState#Player#Actions#Spells#Spell#Concentration
+// 345345345345#GameState#Player#Actions#Spells#Spell#Instant
+// 345345345345#GameState#Player#Actions#Action
+// 345345345345#GameState#Player#Actions#BonusAction
+// 345345345345#GameState#Player#Actions#Reaction
