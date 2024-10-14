cd87ce66337784e77b79f4b998ac111ef151aa3a
Forrest Miller <forrestmillerj@gmail.com>
2024-10-14 15:53:59 -0500

    game sort key tests

---
+++ src/game/src/state/game/enemy/mod.rs
@@ -0,0 +1,4 @@
+// #[derive(strum::Display)]
+// pub enum EnemySortKey {
+//     // #[strum(to_string = "Inventory#{0}")]
+// }
--- src/game/src/state/game/mod.rs
+++ src/game/src/state/game/mod.rs
@@ -1,10 +1,10 @@
-use player::PlayerSortKey;
-
 pub mod enemy;
 pub mod level;
 pub mod npc;
 pub mod player;
 pub mod round;
+
+use player::PlayerSortKey;
 
 #[derive(strum::Display)]
 pub enum GameSortKey {
@@ -14,8 +14,54 @@ pub enum GameSortKey {
     Enemy(PlayerSortKey),
     #[strum(to_string = "NPC#{0}")]
     NPC(PlayerSortKey),
-    #[strum(to_string = "Level#{0}")]
-    Level(PlayerSortKey),
-    #[strum(to_string = "Round#{0}")]
-    Round(PlayerSortKey),
+    #[strum(to_string = "Level")]
+    Level,
+    #[strum(to_string = "Round")]
+    Round,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::GameSortKey;
+    use super::super::RootSortKey;
+    use super::player::{stats::abilities::AbilitiesSortKey, stats::StatsSortKey};
+    use super::PlayerSortKey;
+
+    #[test]
+    fn test_game_sort_key() {
+        // maybe use EnumIter here, initial exploration did not work b/c of
+        // having to use a default
+        let game_id = "12345";
+        let variants = vec![
+            (
+                "12345#Game#Enemy#Stats#Abilities#Strength",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Enemy(PlayerSortKey::Stats(StatsSortKey::Abilities(
+                        AbilitiesSortKey::Strength,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#NPC#Stats#Abilities#Strength",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::NPC(PlayerSortKey::Stats(StatsSortKey::Abilities(
+                        AbilitiesSortKey::Strength,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Level",
+                RootSortKey::Game(game_id, GameSortKey::Level),
+            ),
+            (
+                "12345#Game#Round",
+                RootSortKey::Game(game_id, GameSortKey::Round),
+            ),
+        ];
+        for variant in variants.iter() {
+            assert_eq!(variant.0, variant.1.to_string())
+        }
+    }
 }
