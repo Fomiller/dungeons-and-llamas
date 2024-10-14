d45f555b7141246f408fa690acdeb733079b7364
Forrest Miller <forrestmillerj@gmail.com>
2024-10-14 14:53:27 -0500

    skills sort key tests

--- src/game/src/state/game/player/stats/mod.rs
+++ src/game/src/state/game/player/stats/mod.rs
@@ -1,27 +1,27 @@
 pub mod abilities;
-pub mod skills;
-pub mod core_attributes;
 pub mod conditions;
+pub mod core_attributes;
 pub mod saving_throws;
+pub mod skills;
 
-use skills::SkillsSortKey;
-use saving_throws::SavingThrowsSortKey;
-use core_attributes::CoreAttributesSortKey;
 use abilities::AbilitiesSortKey;
 use conditions::ConditionsSortKey;
+use core_attributes::CoreAttributesSortKey;
+use saving_throws::SavingThrowsSortKey;
+use skills::SkillsSortKey;
 
 #[derive(strum::Display)]
 pub enum StatsSortKey {
-    #[strum(to_string = "#Skills#{0}")]
+    #[strum(to_string = "Skills#{0}")]
     Skills(SkillsSortKey),
-    #[strum(to_string = "#SavingThrows#{0}")]
+    #[strum(to_string = "SavingThrows#{0}")]
     SavingThrows(SavingThrowsSortKey),
-    #[strum(to_string = "#CoreAttributes#{0}")]
+    #[strum(to_string = "CoreAttributes#{0}")]
     CoreAttributes(CoreAttributesSortKey),
-    #[strum(to_string = "#Abilities#{0}")]
+    #[strum(to_string = "Abilities#{0}")]
     Abilities(AbilitiesSortKey),
-    #[strum(to_string = "#Conditions#{0}")]
+    #[strum(to_string = "Conditions#{0}")]
     Conditions(ConditionsSortKey),
-    #[strum(to_string = "#Defenses")]
+    #[strum(to_string = "Defenses")]
     Defenses(),
 }
--- src/game/src/state/game/player/stats/skills/mod.rs
+++ src/game/src/state/game/player/stats/skills/mod.rs
@@ -1,37 +1,211 @@
 #[derive(strum::Display)]
 pub enum SkillsSortKey {
-    #[strum(to_string = "#Acrobatics")]
+    #[strum(to_string = "Acrobatics")]
     Acrobatics,
-    #[strum(to_string = "#AnimalHandling")]
+    #[strum(to_string = "AnimalHandling")]
     AnimalHandling,
-    #[strum(to_string = "#Arcana")]
+    #[strum(to_string = "Arcana")]
     Arcana,
-    #[strum(to_string = "#Atheletics")]
+    #[strum(to_string = "Atheletics")]
     Atheletics,
-    #[strum(to_string = "#Deception")]
+    #[strum(to_string = "Deception")]
     Deception,
-    #[strum(to_string = "#History")]
+    #[strum(to_string = "History")]
     History,
-    #[strum(to_string = "#Insight")]
+    #[strum(to_string = "Insight")]
     Insight,
-    #[strum(to_string = "#Intimidatiaon")]
+    #[strum(to_string = "Intimidatiaon")]
     Intimidatiaon,
-    #[strum(to_string = "#Investigation")]
+    #[strum(to_string = "Investigation")]
     Investigation,
-    #[strum(to_string = "#Medicine")]
+    #[strum(to_string = "Medicine")]
     Medicine,
-    #[strum(to_string = "#Nature")]
+    #[strum(to_string = "Nature")]
     Nature,
-    #[strum(to_string = "#Perception")]
+    #[strum(to_string = "Perception")]
     Perception,
-    #[strum(to_string = "#Persuasion")]
+    #[strum(to_string = "Persuasion")]
     Persuasion,
-    #[strum(to_string = "#Religion")]
+    #[strum(to_string = "Religion")]
     Religion,
-    #[strum(to_string = "#SleightOfHand")]
+    #[strum(to_string = "SleightOfHand")]
     SleightOfHand,
-    #[strum(to_string = "#Stealth")]
+    #[strum(to_string = "Stealth")]
     Stealth,
-    #[strum(to_string = "#Stealth")]
+    #[strum(to_string = "Survival")]
     Survival,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::super::super::RootSortKey;
+    use super::super::super::super::GameSortKey;
+    use super::super::super::PlayerSortKey;
+    use super::super::StatsSortKey;
+    use super::SkillsSortKey;
+
+    #[test]
+    fn test_skills_sort_key() {
+        // maybe use EnumIter here, initial exploration did not work b/c of
+        // having to use a default
+        let game_id = "12345";
+        let variants = vec![
+            (
+                "12345#Game#Player#Stats#Skills#Acrobatics",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Acrobatics,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#AnimalHandling",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::AnimalHandling,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Arcana",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Arcana,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Atheletics",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Atheletics,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Deception",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Deception,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#History",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::History,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Insight",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Insight,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Intimidatiaon",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Intimidatiaon,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Investigation",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Investigation,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Medicine",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Medicine,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Nature",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Nature,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Perception",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Perception,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Persuasion",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Persuasion,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Religion",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Religion,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#SleightOfHand",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::SleightOfHand,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Stealth",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Stealth,
+                    ))),
+                ),
+            ),
+            (
+                "12345#Game#Player#Stats#Skills#Survival",
+                RootSortKey::Game(
+                    game_id,
+                    GameSortKey::Player(PlayerSortKey::Stats(StatsSortKey::Skills(
+                        SkillsSortKey::Survival,
+                    ))),
+                ),
+            ),
+        ];
+        for variant in variants.iter() {
+            assert_eq!(variant.0, variant.1.to_string())
+        }
+    }
 }
