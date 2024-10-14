1b39460d90a0f7bb7f2f1026a151ba9198f6c1ce
Forrest Miller <forrestmillerj@gmail.com>
2024-10-14 15:42:07 -0500

    init submodules

--- src/game/src/state/game/mod.rs
+++ src/game/src/state/game/mod.rs
@@ -1,6 +1,10 @@
 use player::PlayerSortKey;
 
+pub mod enemy;
+pub mod level;
+pub mod npc;
 pub mod player;
+pub mod round;
 
 #[derive(strum::Display)]
 pub enum GameSortKey {
@@ -8,8 +12,10 @@ pub enum GameSortKey {
     Player(PlayerSortKey),
     #[strum(to_string = "Enemy#{0}")]
     Enemy(PlayerSortKey),
-    #[strum(to_string = "Level#{0}")]
-    Level(PlayerSortKey),
     #[strum(to_string = "NPC#{0}")]
     NPC(PlayerSortKey),
+    #[strum(to_string = "Level#{0}")]
+    Level(PlayerSortKey),
+    #[strum(to_string = "Round#{0}")]
+    Round(PlayerSortKey),
 }
