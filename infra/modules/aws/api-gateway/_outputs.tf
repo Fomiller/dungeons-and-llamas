89c4158981fa9b91c030525ef614ef5fb9e9e83e
Forrest Miller <forrestmillerj@gmail.com>
2024-10-21 11:40:19 -0500

    apigw outputs

--- infra/modules/aws/api-gateway/_outputs.tf
+++ infra/modules/aws/api-gateway/_outputs.tf
@@ -1,3 +1,7 @@
-output "base_url" {
+output "discord_bot_url" {
   value = aws_api_gateway_deployment.example.invoke_url
+}
+
+output "dnl_api_url" {
+  value = aws_api_gateway_deployment.dnl_api.invoke_url
 }
