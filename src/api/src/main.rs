ea1aa6fa1136e83cc5d39f3246628b1ba402261e
Forrest Miller <forrestmillerj@gmail.com>
2024-10-23 08:55:42 -0500

    using router for api endpoints

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,41 +1,27 @@
+use axum::http::StatusCode;
+use axum::{
+    extract::Path,
+    response::Json,
+    routing::{get, post},
+    Router,
+};
 use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    println!("{:?}", event);
-    println!("{:?}", &event.uri());
-    let (parts, body) = event.clone().into_parts();
-    println!("Path: {:?}", parts.uri.path());
-    println!("Host: {:?}", parts.uri.host());
-    println!("Scheme: {:?}", parts.uri.scheme_str());
-    println!("Path and Query: {:?}", parts.uri.path_and_query());
-
-    let who = &event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .header("Access-Control-Allow-Headers", "Content-Type,Authorization")
-        .header("Access-Control-Allow-Origin", "*")
-        .header("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
 
-    run(service_fn(function_handler)).await
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/api", get(get_api));
+
+    run(app).await
+}
+async fn root() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /" }))
+}
+
+async fn get_api() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /api" }))
 }
