d1f2d4ca9cbfef844a6469e13152e1b4f6c3510a
Forrest Miller <forrestmillerj@gmail.com>
2024-10-25 10:10:44 -0500

    cleanup; moved /api/new-game -> api/game/new

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -7,49 +7,34 @@ use axum::{
 use axum_macros::debug_handler;
 use game::client::Client;
 use lambda_http::{run, tracing, Error};
-use serde_json::{json, Value};
+use serde_json::json;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct User {
+    id: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/api/new-game", post(post_new_game_handler));
+    let api_router = Router::new().route("/game/new", post(post_new_game_handler));
 
-    // let api_router = Router::new()
-    //     .route("/hello", get(hello_from_the_server))
-    //     .route("/todos", post(add_todo))
-
-    // let app = Router::new()
-    //     .nest("/api", api_router)
-    //     .layer(CorsLayer::permissive())
-    //     .route("/", get(hello))
-    //     .route("/todos", get(another_page))
-    //     .route("/dashboard", get(dashboard_page))
-    //     .with_state(Arc::clone(&state))
-    //     .nest_service(
-    //         "/assets",
-    //         ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-    //     )
-    //     .with_state(Arc::clone(&state));
+    let app = Router::new().route("/", get(root)).nest("/api", api_router);
 
     run(app).await
 }
 
-async fn root() -> Json<Value> {
-    Json(json!({ "msg": "I am GET /" }))
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct User {
-    user_id: String,
+#[debug_handler]
+async fn root() -> impl IntoResponse {
+    StatusCode::NOT_FOUND
 }
 
 #[debug_handler]
 async fn post_new_game_handler(Json(payload): Json<User>) -> impl IntoResponse {
     let client = Client::new().await;
-    match client.try_new_game(&payload.user_id).await {
+
+    match client.try_new_game(&payload.id).await {
         Ok(_) => (
             StatusCode::CREATED,
             Json(json!({"detail": "new game created"})),
@@ -57,39 +42,8 @@ async fn post_new_game_handler(Json(payload): Json<User>) -> impl IntoResponse {
             .into_response(),
         Err(e) => (
             StatusCode::BAD_REQUEST,
-            Json(json!({"error": format!("error creating new game: {}",e)})),
+            Json(json!({"error": format!("{}",e)})),
         )
             .into_response(),
     }
-    // // Ok(Json())
-    // let body = json!({
-    //     "details": format!("new game created"),  // Include the error details in the response
-    // });
-    // Ok(LambdaResponse::builder()
-    //     .status(StatusCode::OK)
-    //     .body(Body::Text(body.to_string()))
-    //     .unwrap())
 }
-
-// async fn post_new_game_wrapper(
-//     payload: Json<User>,
-// ) -> anyhow::Result<impl IntoResponse, (StatusCode, Json<Value>)> {
-//     match post_new_game_handler(payload).await {
-//         Ok(response) => response,
-//         Err(err) => {
-//             eprintln!("Error creating new game: {:?}", err);
-//
-//             // Return a JSON error response
-//             let error_body = json!({
-//                 "error": "Failed to create new game",
-//                 "details": format!("{}", err),  // Include the error details in the response
-//             });
-//
-//             LambdaResponse::builder()
-//                 .status(StatusCode::BAD_REQUEST) // 400 Bad Request for invalid input
-//                 .header("Content-Type", "application/json") // Set the response as JSON
-//                 .body(Body::Text(error_body.to_string())) // Serialize the JSON response
-//                 .unwrap()
-//         }
-//     }
-// }
