11f42001f05bba5be736cd0c7e563061b3c44ee6
Forrest Miller <forrestmillerj@gmail.com>
2024-12-06 11:06:24 -0600

    fix: output values

--- infra/modules/aws/security/_outputs.tf
+++ infra/modules/aws/security/_outputs.tf
@@ -1,7 +1,7 @@
 output "aws_security_group_id_lambda_basic" {
-  value = aws_security_group_id.lambda_basic_sg.id
+  value = aws_security_group.lambda_basic_sg.id
 }
 
 output "aws_security_group_id_efs" {
-  value = aws_security_group_id.efs_sg.id
+  value = aws_security_group.efs_sg.id
 }
