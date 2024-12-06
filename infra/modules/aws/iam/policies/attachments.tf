abe74e72ec53916339c9af8b82fc1cf6140bd6e5
Forrest Miller <forrestmillerj@gmail.com>
2024-12-06 14:38:47 -0600

    iam policy changes for llmhandler lambdas

--- infra/modules/aws/iam/policies/attachments.tf
+++ infra/modules/aws/iam/policies/attachments.tf
@@ -42,3 +42,9 @@ resource "aws_iam_role_policy_attachment" "lambda_llm_handler_s3" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   role       = var.iam_role_name_lambda_llm_handler
 }
+
+
+resource "aws_iam_role_policy_attachment" "lambda_llm_handler_vpc_access" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  role       = var.iam_role_name_lambda_llm_handler
+}
