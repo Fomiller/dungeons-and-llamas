ef4da0bd6b57c76345ec878e75b67175833f3266
Forrest Miller <forrestmillerj@gmail.com>
2024-12-16 10:54:17 -0600

    first pass at refactor

--- src/llmhandler/src/main.rs
+++ src/llmhandler/src/main.rs
@@ -1,10 +1,6 @@
 use db::models::*;
-use db::schema::*;
-use diesel::prelude::*;
-use diesel::query_dsl::RunQueryDsl;
-use diesel::Connection;
-use diesel::PgConnection;
-use pgvector::{Vector, VectorExpressionMethods};
+use db::*;
+use pgvector::Vector;
 use serde_json::Value;
 
 use std::env;
@@ -33,6 +29,7 @@ struct Response {
     message: String,
     output: Option<String>,
 }
+
 #[derive(Debug)]
 struct BedrockConverseError(String);
 impl std::fmt::Display for BedrockConverseError {
@@ -40,12 +37,15 @@ impl std::fmt::Display for BedrockConverseError {
         write!(f, "Can't invoke. Reason: {}", self.0)
     }
 }
+
 impl std::error::Error for BedrockConverseError {}
+
 impl From<&str> for BedrockConverseError {
     fn from(value: &str) -> Self {
         BedrockConverseError(value.to_string())
     }
 }
+
 impl From<&ConverseError> for BedrockConverseError {
     fn from(value: &ConverseError) -> Self {
         BedrockConverseError::from(match value {
@@ -55,150 +55,82 @@ impl From<&ConverseError> for BedrockConverseError {
         })
     }
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
+}
+
 async fn call_bedrock(
     client: &BedrockClient,
     model_id: &str,
-    prompt: &str,
-    context: &str,
-    instructions: &str,
-    user: &str,
-    game: &str,
+    prompt: String,
+    context: String,
+    instructions: String,
+    user_id: String,
+    game_id: String,
 ) -> anyhow::Result<String> {
-    let mut conn = connect_to_database().await;
+    let mut conn = db::connect_to_database().await;
     let embedding_dimension = 256;
 
-    let embed_body = serde_json::json!({
-        "inputText": context,
-        "dimensions": embedding_dimension,
-        "normalize": true
-    });
+    let vector_context =
+        Vector::from(try_create_embed(client, &context, embedding_dimension, true).await?);
 
-    let embed_body_prompt = serde_json::json!({
-        "inputText": prompt,
-        "dimensions": embedding_dimension,
-        "normalize": true
-    });
-
-    let embed_response = client
-        .invoke_model()
-        .model_id("amazon.titan-embed-text-v2:0")
-        .body(embed_body.to_string().into_bytes().into())
-        .accept("application/json")
-        .content_type("application/json")
-        .send()
-        .await;
-
-    let embed_response_prompt = client
-        .invoke_model()
-        .model_id("amazon.titan-embed-text-v2:0")
-        .body(embed_body_prompt.to_string().into_bytes().into())
-        .accept("application/json")
-        .content_type("application/json")
-        .send()
-        .await;
-
-    let embedding = match embed_response {
-        Ok(output) => {
-            // Convert response bytes into a String
-            let output_string = String::from_utf8(output.body.into_inner())
-                .expect("Response body is not valid UTF-8");
-            // println!("{}", output_string);
-
-            // Parse JSON string into a serde_json::Value
-            let json_response: serde_json::Value =
-                serde_json::from_str(&output_string).expect("Response body is not valid JSON");
-
-            // Extract relevant fields from the JSON response
-            let embedding = json_response.get("embedding").unwrap();
-            Ok(embedding.clone())
-        }
-        Err(e) => {
-            println!("{:?}", e.as_service_error());
-            Err(anyhow::anyhow!("{:?}", e.as_service_error()))
-        }
-    }?;
-
-    let embedding_prompt = match embed_response_prompt {
-        Ok(output) => {
-            // Convert response bytes into a String
-            let output_string = String::from_utf8(output.body.into_inner())
-                .expect("Response body is not valid UTF-8");
-            // println!("{}", output_string);
-
-            // Parse JSON string into a serde_json::Value
-            let json_response: serde_json::Value =
-                serde_json::from_str(&output_string).expect("Response body is not valid JSON");
-
-            // Extract relevant fields from the JSON response
-            let embedding = json_response.get("embedding").unwrap();
-            Ok(embedding.clone())
-        }
-        Err(e) => {
-            println!("{:?}", e.as_service_error());
-            Err(anyhow::anyhow!("{:?}", e.as_service_error()))
-        }
-    }?;
-
-    let embedding_data = Vector::from(value_to_f32_slice(&embedding)?);
-    let embedding_data_prompt = Vector::from(value_to_f32_slice(&embedding_prompt)?);
+    let vector_prompt =
+        Vector::from(try_create_embed(client, &prompt, embedding_dimension, true).await?);
 
     // write context to database
-    write_to_database(&mut conn, embedding_data, user, game, context, "output").await?;
+    let ctx_embed = NewEmbedding {
+        vector: vector_context,
+        user_id: &user_id,
+        game_id: &game_id,
+        text: context.to_string(),
+        type_: "output".to_string(),
+    };
+    try_insert_new_embedding(&mut conn, &ctx_embed).await?;
+
     // write prompt to database
-    write_to_database(
+    let prompt_embed = NewEmbedding {
+        vector: vector_prompt,
+        user_id: &user_id,
+        game_id: &game_id,
+        text: prompt.to_string(),
+        type_: "prompt".to_string(),
+    };
+    try_insert_new_embedding(&mut conn, &prompt_embed).await?;
+
+    let neighbors = db::try_similarity_search(
         &mut conn,
-        embedding_data_prompt.clone(),
-        user,
-        game,
-        prompt,
-        "prompt",
+        5,
+        prompt_embed.vector,
+        prompt_embed.user_id,
+        prompt_embed.game_id,
+        false,
     )
     .await?;
 
-    let neighbors =
-        similarity_search(&mut conn, 5, embedding_data_prompt.clone(), user, game).await?;
+    tracing::info!("Neighbors: {:?}", neighbors);
 
-    println!("Neighbors: {:?}", neighbors);
+    let contexts = get_context_from_neighbors(neighbors);
 
-    let embedding_contexts = neighbors
-        .into_iter()
-        .map(|e| format!("\n{}\n", e.text))
-        .collect::<Vec<String>>();
+    let input = create_input(contexts, instructions, prompt);
 
-    println!("Embedding Contexts: {:?}", embedding_contexts);
-
-    let mut input_context = String::new();
-    for context in embedding_contexts {
-        input_context.push_str(&context)
-    }
-
-    let input = format!(
-        "{}\n<Context>{}</Context>\n<Prompt>\n{}\n</Prompt>",
-        instructions, input_context, prompt
-    );
-    println!("INPUT: {}", input);
-
-    let response = client
-        .converse()
-        .model_id(model_id)
-        .system(SystemContentBlock::Text(
-            "You are a Dungeons and Dragons Dungeon Master.".to_string(),
-        ))
-        .messages(
-            Message::builder()
-                .role(ConversationRole::User)
-                .content(ContentBlock::Text(input.to_string()))
-                .build()?,
-        )
-        .send()
-        .await;
+    //TODO reafactor this
+    let message = Message::builder()
+        .role(ConversationRole::User)
+        .content(ContentBlock::Text(input.to_string()))
+        .build()?;
+    let system = "You are a Dungeons and Dragons Dungeon Master.".to_string();
+    let response = converse(client, model_id, system, message).await;
 
     match response {
         Ok(output) => {
             let text = get_converse_output_text(output)?;
             Ok(text)
         }
-        Err(e) => Err(anyhow::anyhow!("{:?}", e.as_service_error())),
+        Err(e) => Err(anyhow::anyhow!("{}", e)),
     }
 }
 
@@ -227,11 +159,11 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
     match call_bedrock(
         &bedrock_client,
         &model_id,
-        &payload.prompt,
-        &payload.context,
-        &payload.instructions,
-        &payload.user_id,
-        &payload.game_id,
+        payload.prompt,
+        payload.context,
+        payload.instructions,
+        payload.user_id,
+        payload.game_id,
     )
     .await
     {
@@ -244,13 +176,6 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
             output: None,
         }),
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
-
-    run(service_fn(function_handler)).await
 }
 
 fn value_to_f32_slice(value: &Value) -> anyhow::Result<Vec<f32>> {
@@ -271,67 +196,89 @@ fn value_to_f32_slice(value: &Value) -> anyhow::Result<Vec<f32>> {
     }
 }
 
-async fn connect_to_database() -> PgConnection {
-    let port = 5432;
-    let rds_user = env::var("RDS_USERNAME").expect("RDS_USERNAME must be set");
-    let rds_pass = env::var("RDS_PASSWORD").expect("RDS_PASSWORD must be set");
-    let database_endpoint = env::var("DATABASE_ENDPOINT").expect("DATABASE_ENDPOINT must be set");
-    let database_name = env::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
-    let database_url = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        rds_user, rds_pass, database_endpoint, port, database_name
-    );
+async fn try_create_embed(
+    client: &BedrockClient,
+    input_text: &str,
+    dimensions: i64,
+    normalize: bool,
+) -> anyhow::Result<Vec<f32>> {
+    let body = serde_json::json!({
+        "inputText": input_text,
+        "dimensions": dimensions,
+        "normalize": normalize
+    });
 
-    let connection = PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    let res = client
+        .invoke_model()
+        .model_id("amazon.titan-embed-text-v2:0")
+        .body(body.to_string().into_bytes().into())
+        .accept("application/json")
+        .content_type("application/json")
+        .send()
+        .await;
 
-    connection
-}
+    let embedding_prompt = match res {
+        Ok(output) => {
+            // Convert response bytes into a String
+            let output_string = String::from_utf8(output.body.into_inner())
+                .expect("Response body is not valid UTF-8");
 
-async fn write_to_database(
-    conn: &mut PgConnection,
-    embedding: Vector,
-    user_id: &str,
-    game_id: &str,
-    text: &str,
-    type_: &str,
-) -> anyhow::Result<Embedding> {
-    let new_embedding = NewEmbedding {
-        embedding,
-        user_id: user_id.to_string(),
-        game_id: game_id.to_string(),
-        text: text.to_string(),
-        type_: type_.to_string(),
+            // Parse JSON string into a serde_json::Value
+            let json_response: serde_json::Value =
+                serde_json::from_str(&output_string).expect("Response body is not valid JSON");
+
+            // Extract relevant fields from the JSON response
+            let embedding = json_response.get("embedding").unwrap();
+            let x = value_to_f32_slice(embedding);
+            Ok(x)
+        }
+        Err(e) => {
+            println!("{:?}", e.as_service_error());
+            Err(anyhow::anyhow!("{:?}", e.as_service_error()))
+        }
     };
 
-    match diesel::insert_into(embeddings::table)
-        .values(&new_embedding)
-        .returning(Embedding::as_returning())
-        .get_result(conn)
-    {
-        Ok(e) => Ok(e),
-        Err(_) => Err(anyhow::anyhow!("Error inserting Embedding")),
-    }
+    embedding_prompt.unwrap()
 }
 
-async fn similarity_search(
-    conn: &mut PgConnection,
-    limit: i64,
-    embedding: Vector,
-    user_id: &str,
-    game_id: &str,
-) -> anyhow::Result<Vec<Embedding>> {
-    let query = embeddings::table
-        .filter(embeddings::user_id.eq(user_id))
-        .filter(embeddings::game_id.eq(game_id))
-        .filter(embeddings::type_.eq("output"))
-        .order(embeddings::embedding.l2_distance(embedding))
-        .limit(limit)
-        .select(Embedding::as_select());
+fn get_context_from_neighbors(neighbors: Vec<Embedding>) -> Vec<String> {
+    let contexts = neighbors
+        .into_iter()
+        .map(|e| format!("\n{}\n", e.text))
+        .collect::<Vec<String>>();
 
-    // let debug = debug_query::<diesel::pg::Pg, _>(&query);
-    // println!("QUERY: {:?}", debug);
+    tracing::debug!("Embedding Contexts: {:?}", contexts);
+    contexts
+}
 
-    let neighbors = query.load(conn).expect("Error finding neighbors");
-    Ok(neighbors)
+fn create_input(contexts: Vec<String>, instructions: String, prompt: String) -> String {
+    let mut input_context = String::new();
+    for context in contexts {
+        input_context.push_str(&context)
+    }
+    let input = format!(
+        "{}\n<Context>{}</Context>\n<Prompt>\n{}\n</Prompt>",
+        instructions, input_context, prompt
+    );
+
+    tracing::debug!("INPUT: {}", input);
+
+    input
+}
+
+async fn converse(
+    client: &BedrockClient,
+    model_id: &str,
+    system: String,
+    message: Message,
+) -> anyhow::Result<ConverseOutput> {
+    let response = client
+        .converse()
+        .model_id(model_id)
+        .system(SystemContentBlock::Text(system))
+        .messages(message)
+        .send()
+        .await?;
+
+    Ok(response)
 }
