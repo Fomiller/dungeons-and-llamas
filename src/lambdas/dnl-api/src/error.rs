6cbca64c3068931f60d33856a9837cfd5b63e2c3
Forrest Miller <forrestmillerj@gmail.com>
2025-03-25 11:12:23 -0500

    apiErrorEnum and dependency cleanup with cargo machete

--- src/lambdas/dnl-api/src/error.rs
+++ src/lambdas/dnl-api/src/error.rs
@@ -1,9 +1,50 @@
+use aws_sdk_dynamodb::error::SdkError;
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Json, Response},
 };
 use dnl_types::api::error::ApiResponseError;
 use serde_json::json;
+
+#[derive(thiserror::Error, Debug, serde::Serialize, serde::Deserialize)]
+pub enum ApiErrorEnum {
+    #[error("There was an error with the Store")]
+    StoreError,
+    #[error("There was an error with the Llm process")]
+    LlmError,
+    #[error("Unexpected error: {0}")]
+    Unexpected(String),
+    #[error("There was an error with the aws sdk: {0}")]
+    AwsError(String),
+}
+
+// Custom From<anyhow::Error> because anyhow does no implement Serialize/Deserialize
+impl From<anyhow::Error> for ApiErrorEnum {
+    fn from(err: anyhow::Error) -> Self {
+        ApiErrorEnum::Unexpected(err.to_string())
+    }
+}
+
+// Blanket implementation of generic AWS SdkError conversion
+impl<T> From<SdkError<T>> for ApiErrorEnum
+where
+    T: std::fmt::Display,
+{
+    fn from(err: SdkError<T>) -> Self {
+        ApiErrorEnum::AwsError(format!("{}", err))
+    }
+}
+
+// :TODO: handle different variants here
+impl IntoResponse for ApiErrorEnum {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"error": format!("{}", self)})),
+        )
+            .into_response()
+    }
+}
 
 // https://github.com/tokio-rs/axum/blob/main/examples/anyhow-error-response/src/main.rs
 // Make our own error that wraps `anyhow::Error`.
